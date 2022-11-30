4f96804659aebab0fcaaa48a5617a3beb842897d
hdlr <95166106+hhd2009@users.noreply.github.com>
2022-11-30 08:54:08 +0800

    Update go_to_register.js

--- assets/js/go_to_register.js
+++ assets/js/go_to_register.js
@@ -1,2 +1,4 @@
-window.open('http://lantian.ink:48101/register/')
-location.href = 'https://hhd2009.github.io/lty'
+function go_to_register{
+  window.open('http://lantian.ink:48101/register/')
+  location.href = 'https://hhd2009.github.io/lty'
+}
