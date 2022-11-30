4117c3005225ae0e527d8e58a63cc9de286f5039
hdlr <95166106+hhd2009@users.noreply.github.com>
2022-11-30 09:40:08 +0800

    Update img.js

--- assets/js/img.js
+++ assets/js/img.js
@@ -5,15 +5,15 @@
         bodyBgs[2] = "./assets/img/3.jpg";
         bodyBgs[3] = "./assets/img/4.jpg";
         bodyBgs[4] = "./assets/img/5.jpg";
-        bodyBgs[5] = "/assets/img/6.jpg";
-        //bodyBgs[6] = "/assets/img/7.jpg";
-        //bodyBgs[7] = "/assets/img/8.jpg";
-        //bodyBgs[8] = "/assets/img/9.jpg";
-        //bodyBgs[9] = "/assets/img/10.jpg";
-        //bodyBgs[10] = "/assets/img/11.jpg";
-        //bodyBgs[11] = "/assets/img/12.jpg";
-        //bodyBgs[12] = "/assets/img/13.jpg";
-        //bodyBgs[13] = "/assets/img/14.jpg";
+        bodyBgs[5] = "./assets/img/6.jpg";
+        //bodyBgs[6] = "./assets/img/7.jpg";
+        //bodyBgs[7] = "./assets/img/8.jpg";
+        //bodyBgs[8] = "./assets/img/9.jpg";
+        //bodyBgs[9] = "./assets/img/10.jpg";
+        //bodyBgs[10] = "./assets/img/11.jpg";
+        //bodyBgs[11] = "./assets/img/12.jpg";
+        //bodyBgs[12] = "./assets/img/13.jpg";
+        //bodyBgs[13] = "./assets/img/14.jpg";
 
         var randomBgIndex = Math.round( Math.random() * 5 );
 
